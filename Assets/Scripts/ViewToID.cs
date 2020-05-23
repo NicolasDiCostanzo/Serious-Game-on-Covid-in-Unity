a036d309e413a3dcb356319c273627b8e7ee9c28
Nicolas-lgtm <n.di-costanzo@ludus-academie.com>
2020-05-23 23:32:38 +0200

    décors + début caméra sur ID

---
+++ Assets/Scripts/ViewToID.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ViewToID : MonoBehaviour
+{
+    CinemachineVirtualCamera DeskCam;
+
+    void Awake()
+    {
+        DeskCam = GameObject.Find("DeskCam").GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void OnEnable()
+    {
+        DeskCam.Priority *= -1;
+        enabled = false;
+    }
+}
